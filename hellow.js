55093fd20984700e849b8d7433e8047ca3e827c7
praise <praisetaiwo24@gmail.com>
2023-09-18 17:01:34 +0100

    content update

--- hellow.js
+++ hellow.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => res.send('Hello CI/CD'))
+app.get('/', (req, res) => res.send('Hello world, good'))
 app.listen(3000, () => console.log('Node.js app listening on port 3000.'))
